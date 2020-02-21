ce218e200344457c6e749042485ee4802af93435
Vincentz1911 <henrik_v_poulsen@hotmail.com>
2020-02-21 14:24:51 +0100

    v1

---
+++ app/src/main/java/com/vincentz1911/mapsandbtandobd2/MapsActivity.java
@@ -0,0 +1,64 @@
+package com.vincentz1911.mapsandbtandobd2;
+
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.os.Bundle;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
+    private Fragment fragment;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps);
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+
+        //OBD2Fragment obd2Fragment = (OBD2Fragment) getSupportFragmentManager().findFragmentById(R.id.layout_obd2);
+
+
+        FragmentManager fm = getSupportFragmentManager();
+        fragment = fm.findFragmentByTag("obd2tag");
+        if (fragment == null) {
+            FragmentTransaction ft = fm.beginTransaction();
+            fragment = new OBD2Fragment();
+            ft.add(R.id.layout_obd2, fragment,"obd2tag").commit();
+            //ft.commit();
+        }
+
+    }
+
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        // Add a marker in Sydney and move the camera
+        LatLng sydney = new LatLng(-34, 151);
+        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+    }
+}
---
+++ app/src/main/java/com/vincentz1911/mapsandbtandobd2/OBD2Fragment.java
@@ -0,0 +1,160 @@
+package com.vincentz1911.mapsandbtandobd2;
+
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothSocket;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.fragment.app.Fragment;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Set;
+import java.util.UUID;
+
+import com.vincentz1911.mapsandbtandobd2.obd.commands.*;
+import com.vincentz1911.mapsandbtandobd2.obd.commands.control.*;
+import com.vincentz1911.mapsandbtandobd2.obd.commands.engine.*;
+import com.vincentz1911.mapsandbtandobd2.obd.commands.fuel.*;
+import com.vincentz1911.mapsandbtandobd2.obd.commands.pressure.*;
+import com.vincentz1911.mapsandbtandobd2.obd.commands.protocol.*;
+import com.vincentz1911.mapsandbtandobd2.obd.commands.temperature.*;
+
+
+public class OBD2Fragment extends Fragment {
+
+    private String speed, rpm, fuelLevel, oilTemp, consumption;
+    private TextView txt_speed, txt_rpm;
+    private Thread getInfoThread, updateInfoThread;
+    private SharedPreferences sharedPref;
+    private String deviceAddress;
+    private BluetoothSocket socket = null;
+
+    public OBD2Fragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_obd2, container, false);
+        txt_speed = view.findViewById(R.id.txt_speed);
+        txt_rpm = view.findViewById(R.id.txt_rpm);
+
+        sharedPref = getActivity().getPreferences(Context.MODE_PRIVATE);
+        deviceAddress = sharedPref.getString("btaddress", "");
+        //if (deviceAddress.equals(""))
+        getBluetoothDevice();
+
+        getInfoThread = new Thread() {
+            public void run() {
+                try {
+                    while (!Thread.currentThread().isInterrupted()) {
+                        Thread.sleep(100);
+                        RPMCommand engineRpmCommand = new RPMCommand();
+                        SpeedCommand speedCommand = new SpeedCommand();
+
+                        OilTempCommand oilTempCommand = new OilTempCommand();
+                        ConsumptionRateCommand consumptionRateCommand = new ConsumptionRateCommand();
+                        FuelLevelCommand fuelLevelCommand = new FuelLevelCommand();
+
+                        fuelLevelCommand.run(socket.getInputStream(), socket.getOutputStream());
+                        consumptionRateCommand.run(socket.getInputStream(), socket.getOutputStream());
+                        oilTempCommand.run(socket.getInputStream(), socket.getOutputStream());
+                        engineRpmCommand.run(socket.getInputStream(), socket.getOutputStream());
+                        speedCommand.run(socket.getInputStream(), socket.getOutputStream());
+
+                        consumption = consumptionRateCommand.getFormattedResult();
+                        fuelLevel = fuelLevelCommand.getFormattedResult();
+                        oilTemp = oilTempCommand.getFormattedResult();
+                        speed = speedCommand.getFormattedResult();
+                        rpm = engineRpmCommand.getFormattedResult();
+                    }
+                } catch (IOException | InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+
+        updateInfoThread = new Thread() {
+            @Override
+            public void run() {
+                try {
+                    while (!Thread.currentThread().isInterrupted()) {
+                        Thread.sleep(100);
+                        getActivity().runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                txt_speed.setText("speed: " + speed);
+                                txt_rpm.setText("rpm: " + rpm);
+                                // update TextView here!
+                            }
+                        });
+                    }
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+        return view;
+    }
+
+    private void getBluetoothDevice() {
+        ArrayList<String> deviceStrs = new ArrayList<>();
+        final ArrayList<String> devices = new ArrayList<>();
+
+        BluetoothAdapter btAdapter = BluetoothAdapter.getDefaultAdapter();
+        Set<BluetoothDevice> pairedDevices = btAdapter.getBondedDevices();
+        if (pairedDevices.size() > 0) {
+            for (BluetoothDevice device : pairedDevices) {
+                deviceStrs.add(device.getName() + "\n" + device.getAddress());
+                devices.add(device.getAddress());
+            }
+        }
+
+        // show list
+        final AlertDialog.Builder alertDialog = new AlertDialog.Builder(getActivity());
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(getActivity(),
+                android.R.layout.select_dialog_singlechoice, deviceStrs.toArray(new String[0]));
+
+        alertDialog.setSingleChoiceItems(adapter, -1, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                int position = ((AlertDialog) dialog).getListView().getCheckedItemPosition();
+                deviceAddress = devices.get(position);
+                sharedPref.edit().putString("btaddress", deviceAddress).apply();
+                dialog.dismiss();
+                connect();
+            }
+        });
+        alertDialog.setTitle("Choose Bluetooth device");
+        alertDialog.show();
+    }
+
+    private void connect() {
+        try {
+            BluetoothAdapter btAdapter = BluetoothAdapter.getDefaultAdapter();
+            BluetoothDevice device = btAdapter.getRemoteDevice(deviceAddress);
+            UUID uuid = UUID.fromString("00001101-0000-1000-8000-00805F9B34FB");
+            socket = device.createInsecureRfcommSocketToServiceRecord(uuid);
+            socket.connect();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        getInfoThread.start();
+        updateInfoThread.start();
+    }
+}
+
+
+
+
